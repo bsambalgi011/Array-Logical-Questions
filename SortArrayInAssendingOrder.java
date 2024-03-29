cefa6a3162c3d88eb923a213484d2f23ccfe5d15
bsambalgi011 <bsambalgi011@gmail.com>
2024-03-29 16:12:56 +0530

    Logic done

---
+++ SortArrayInAssendingOrder.java
@@ -0,0 +1,12 @@
+package ArrayLogicalQuestions;
+
+import java.util.Arrays;
+
+public class SortArrayInAssendingOrder {
+	public static void main(String[] args) {
+		int [] x= {12, 5,8,9,76,3,4,7};
+		Arrays.sort(x);
+		System.out.println("Sorted Array :- "+Arrays.toString(x));
+	}
+
+}
